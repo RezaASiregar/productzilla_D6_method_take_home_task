48b908e531622e205b544eaf7468406b4313e321
RezaASiregar <rezasiregar98@gmail.com>
2023-03-04 14:40:14 +0700

    inisiasi commit

---
+++ productzilla_D6_method_take_home_task.js
@@ -0,0 +1,207 @@
+//Soal 1 (Procedure)
+let nama1 = 'liverpool'
+let nama2 = 'Manchester City'
+let nama3 = 'Chelsea'
+let nama4 = 'Manchester United'
+let nama5 = 'Tottenham'
+let nama6 = 'Arsenal'
+let nama7 = 'Crystal Palace'
+let nama8 = 'Brighton'
+let nama9 = 'NewCastle'
+let nama10 = 'Nothingham'
+let nama11 = 'Leeds United'
+let nama12 = 'Everton'
+let nama13 = 'Southampton'
+let nama14 = 'Aston Villa'
+let nama15 = 'Brentford FC'
+let nama16 = 'Wolverhampton'
+let nama17 = 'Leicester City'
+let nama18 = 'West Ham'
+let nama19 = 'Fulham'
+let nama20 = 'Bournemouth'
+
+function eplLiverpool(club) {
+    nama1 = club
+}
+
+function eplCity(club) {
+    nama2 = club
+}
+
+function eplChelsea(club) {
+    nama3 = club
+}
+
+function eplMU(club) {
+    nama4 = club
+}
+
+function eplTot(club) {
+    nama5 = club
+}
+
+function eplArs(club) {
+    nama6 = club
+}
+
+function eplCry(club) {
+    nama7 = club
+}
+
+function eplBrg(club) {
+    nama8 = club
+}
+
+function eplNU(club) {
+    nama9 = club
+}
+
+function eplNot(club) {
+    nama10 = club
+}
+
+function eplLeeds(club) {
+    nama11 = club
+}
+
+function eplEverton(club) {
+    nama12 = club
+}
+
+function eplSout(club) {
+    nama13 = club
+}
+
+function eplAston(club) {
+    nama14 = club
+}
+
+function eplBrent(club) {
+    nama15 = club
+}
+
+function eplWolv(club) {
+    nama16 = club
+}
+
+function eplLeic(club) {
+    nama17 = club
+}
+
+function eplWest(club) {
+    nama18 = club
+}
+
+function eplFulham(club) {
+    nama19 = club
+}
+
+function eplBourn(club) {
+    nama20 = club
+}
+
+//Soal 2 (Function)
+function eplL() {
+    return nama1 
+}
+
+function eplC() {
+    return nama2
+}
+
+function eplCh() {
+    return nama3
+}
+
+function eplMa() {
+    return nama4
+}
+
+function eplTo() {
+    return nama5
+}
+
+function eplAr() {
+    return nama6
+}
+
+function eplCr() {
+    return nama7
+}
+
+function eplBr() {
+    return nama8
+}
+
+function eplNe() {
+    return nama9
+}
+
+function eplNo() {
+    return nama10
+}
+
+function eplLe() {
+    return nama11
+}
+
+function eplEv() {
+    return nama12
+}
+
+function eplSo() {
+    return nama13
+}
+
+function eplAs() {
+    return nama14
+}
+
+function eplBre() {
+    return nama15
+}
+
+function eplWo() {
+    return nama16
+}
+
+function eplLei() {
+    return nama17
+}
+
+function eplWes() {
+    return nama18
+}
+
+function eplFul() {
+    return nama19
+}
+
+function eplBo() {
+    return nama20
+}
+
+// Output
+console.log(' == Club Liga Inggris 2022/2023 == ')
+
+console.log(eplL(nama1))
+console.log(eplC(nama2))
+console.log(eplCh(nama3))
+console.log(eplMa(nama4))
+console.log(eplTo(nama5))
+console.log(eplAr(nama6))
+console.log(eplCr(nama7))
+console.log(eplBr(nama8))
+console.log(eplNe(nama9))
+console.log(eplNo(nama10))
+console.log(eplLe(nama11))
+console.log(eplEv(nama12))
+console.log(eplSo(nama13))
+console.log(eplAs(nama14))
+console.log(eplBre(nama15))
+console.log(eplWo(nama16))
+console.log(eplLei(nama17))
+console.log(eplWes(nama18))
+console.log(eplFul(nama19))
+console.log(eplBo(nama20))
+
